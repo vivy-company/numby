924d1c7e1d45b820125fe0f50b1cc5dd22e1ae2f
wiedymi <wiedymi@users.noreply.github.com>
2025-11-15 06:31:07 +0000

    update Homebrew and Scoop to v0.1.3

--- Formula/numby.rb
+++ Formula/numby.rb
@@ -1,32 +1,24 @@
 class Numby < Formula
-  desc "A powerful natural language calculator with a terminal user interface"
-  homepage "https://github.com/wiedymi/numby"
-  version "0.1.0"
+  desc "Powerful natural language calculator with terminal user interface"
+  homepage "https://github.com/vivy-company/numby"
+  version "0.1.3"
+  license "MIT"
 
-  livecheck do
-    url :homepage
-    strategy :github_releases
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wiedymi/numby/releases/download/v#{version}/numby-macos-aarch64"
-    sha256 "TODO: Add SHA256 for aarch64 binary"
-  else
-    url "https://github.com/wiedymi/numby/releases/download/v#{version}/numby-macos-x86_64"
-    sha256 "TODO: Add SHA256 for x86_64 binary"
-  end
-
-  head do
-    url "https://github.com/wiedymi/numby.git"
-    depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/vivy-company/numby/releases/download/v0.1.3/numby-v0.1.3-macos-x86_64.tar.gz"
+      sha256 "552f20fb1cb811c92f7982152c6ce1e06d6c1d0dd18067dafedaa55e8c232f2c"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/vivy-company/numby/releases/download/v0.1.3/numby-v0.1.3-macos-aarch64.tar.gz"
+      sha256 "675c7433efd9a2dd2054becf59c755c7e274eb852407a6422e004b8ae9b6445e"
+    end
   end
 
   def install
-    bin.install "numby"
+    bin.install "numby-v0.1.3-macos-#{Hardware::CPU.arch}" => "numby"
   end
 
   test do
-    assert_match "numby", shell_output("#{bin}/numby --version")
-    assert_equal "5", shell_output("#{bin}/numby '2 + 3'").strip
+    assert_match "numby 0.1.3", shell_output("#{bin}/numby --version")
   end
 end
